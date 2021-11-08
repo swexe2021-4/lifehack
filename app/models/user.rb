289695f6f610ae6969f0f91e0a3049fe47e22255
naoya-3333 <2111141003f@ed.fuk.kindai.ac.jp>
2021-11-08 07:04:09 +0000

    user_validation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    
+    validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+    
+    validates :name, {presence: true,uniqueness: { case_sensitive: false}}
+    
+    validates :gen,:age, {presence: :true}
+    
+    #validates :pass, presence: :true, format: { with: VALID_PASSWORD_REGEX ,message: 'は半角英数を両方含む必要があります'}
+    
+    
 end
