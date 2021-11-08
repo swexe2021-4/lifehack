7f3078c7cc445ed77dad021336a107fce1d35502
daiki <1911140056p@ed.fuk.kindai.ac.jp>
2021-11-08 06:08:45 +0000

    pull

--- app/controllers/lifehacks_controller.rb
+++ app/controllers/lifehacks_controller.rb
@@ -1,2 +1,12 @@
 class LifehacksController < ApplicationController
+    def index
+        @lifehacks = Lifehack.all
+    end
+    def new
+        @lifehack = Lifehack.new
+    end
+    def create
+        @lifehack = Lifehack.new(title: params[:lifehack][:title], exp: params[:lifehack][:exp], file:[:lifehack][:file])
+        @lifehack.save
+    end
 end
